52a507a7ee002347bb4aa704c3b6a0de75d532cf
Raghav Singh <actualrag@gmail.com>
2023-11-02 15:31:44 +0530

    Pattern problems

---
+++ patterns2.java
@@ -0,0 +1,57 @@
+public class patterns2 {
+    public static void main(String[]args ){
+        // Practicing what I learned in last repos
+        //To print
+//        *****
+//        *****
+//        *****
+//        *****
+
+//        int n = 4;
+//        int m = 5;
+//
+//        //outer loop
+//        for(int i = 1; i<=4; i++){
+//            for(int j = 1; j<=5; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//
+//        }
+
+        //works!
+
+        //Patterin 2
+        //*****
+        //*   *
+        //*   *
+        //*****
+
+//        int n = 4;
+//        int m = 5;
+//
+//        //outer loop
+//        for (int i = 1; i<=n; i++){
+//            for (int j = 1; j<=m; j++){
+//                if(i == 1 || j == 1 || i == n || j == m){
+//                    System.out.print("*");
+//                } else {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+        }
+
+
+        // This worked at first tryyy yay!
+
+
+    // Half Pyramid
+
+
+
+
+
+
+    }
+}
